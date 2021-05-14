09025d4cf175f3f2e0e54e4f990f7093849fd88a
Jae Hyun Lee <jminy8@gmail.com>
2021-05-15 00:41:59 +0900

    Add files via upload

---
+++ Models/BertClf.py
@@ -0,0 +1,110 @@
+import argparse
+import random
+import time
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+from copy import deepcopy
+import pandas as pd
+import numpy as np
+
+import transformers
+from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+from torch.utils.data.sampler import BatchSampler
+
+import torchvision
+from torchvision import transforms, datasets
+
+
+#################################################################################################################
+# Reproducible
+#################################################################################################################
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(42)
+random.seed(42)
+os.environ['PYTHONHASHSEED'] = str(42)
+
+
+#################################################################################################################
+# Build Model
+#################################################################################################################
+# Create the BertClassifier class
+class BertClassifier(nn.Module):
+    def __init__(self, opt, freeze_bert=False):
+        """
+        :param opt: hyper parameters
+        :param freeze_bert: set 'False' to fine-tune the BERT model [bool]
+        """
+        super(BertClassifier, self).__init__()
+        # Specify hidden size of BERT, hidden size of our cls, num_class
+        self.bert = BertModel.from_pretrained('bert-base-uncased',
+                                              output_hidden_states=True)
+        self.opt = opt
+        if opt.sent_embedding == 0:
+            D_in, H, D_out = 768, opt.hidden_dim, 5
+        if opt.sent_embedding == 1:
+            D_in, H, D_out = 3072, opt.hidden_dim, 5
+
+        self.classifier = nn.Sequential(nn.Linear(D_in, H), 
+                                        nn.ReLU(),
+                                        nn.Linear(H, D_out))
+
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+
+    def forward(self, input_ids, attention_masks):
+        """
+        Feed input to BERT and the classifier to compute logits
+        
+        :param input_ids: an input tensor with shape (batch_size, max_length) [torch.Tensor]
+        :param attention_masks: a tensor that hold attention mask [torch.Tensor]
+        
+        :return logits: an output tensor with shape (batch_size, num_class) [torch.Tensor]
+        """
+        # Feed input to BERT
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_masks)
+
+        if self.opt.sent_embedding == 0:
+            # Extract the last hidden state of the token '[CLS]'
+            sent_embeddings_tsr = outputs[0][:, 0, :] # [batch_size, 768]
+#             print(f"BertClassifier - forward - sent_embeddings_tsr.shape: {sent_embeddings_tsr.shape}")
+
+        if self.opt.sent_embedding == 1:
+            # Concatenate last 4 layers
+            hidden_states_tuple = outputs[2]  # [13, batch_size, num. tokens, 768], [tuple]
+
+            hidden_states_tsr = torch.stack(hidden_states_tuple,
+                                            dim=0)  # each layer들을 stacking
+            hidden_states_tsr = hidden_states_tsr.permute(1, 0, 2, 3) # [batch_size, 13, num. tokens, 768], [tensor]
+#             print(f"BertClassifier - forward - hidden_states_tsr.shape: {hidden_states_tsr.shape}") 
+
+            hidden_states_list = []  # [batch_size, 768*4]
+            for hidden_state_tsr in hidden_states_tsr:
+#                 print(f"\tBertClassifier - forward - hidden_state_tsr.shape: {hidden_state_tsr.shape}") # [13, num. tokens, 768], [tensor]
+                concated_hidden_state_tsr = torch.cat(
+                    (torch.mean(hidden_state_tsr[-1], dim=0), 
+                     torch.mean(hidden_state_tsr[-2], dim=0),
+                     torch.mean(hidden_state_tsr[-3], dim=0), 
+                     torch.mean(hidden_state_tsr[-4], dim=0)), 
+                    dim=0)
+#                 print(f"\tBertClassifier - forward - concated_hidden_state_tsr.shape: {concated_hidden_state_tsr.shape}") # [4*768], [tensor]
+                hidden_states_list.append(concated_hidden_state_tsr)
+            sent_embeddings_tsr = torch.stack(hidden_states_list, dim=0)
+#             print(f"BertClassifier - forward - sent_embeddings_tsr.shape: {sent_embeddings_tsr.shape}")
+
+        # Feed input to classifier to compute logits
+        logits = self.classifier(sent_embeddings_tsr)
+
+        return logits
---
+++ Models/LstmClf.py
@@ -0,0 +1,87 @@
+import argparse
+import random
+import time
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+from copy import deepcopy
+import pandas as pd
+import numpy as np
+
+import transformers
+from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+from torch.utils.data.sampler import BatchSampler
+
+import torchvision
+from torchvision import transforms, datasets
+
+
+#################################################################################################################
+# Reproducible
+#################################################################################################################
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(42)
+random.seed(42)
+os.environ['PYTHONHASHSEED'] = str(42)
+
+
+#################################################################################################################
+# Build Model
+#################################################################################################################
+# Create the LstmClassifier class
+class LstmClassifier(nn.Module):
+    def __init__(self, opt, vocab_size):
+        """
+        :param opt: hyper parameters
+        """
+        super(LstmClassifier, self).__init__()
+
+        self.embedding = nn.Embedding(vocab_size, opt.max_len)
+        self.lstm = nn.LSTM(input_size=opt.max_len,
+                            hidden_size=opt.hidden_dim,
+                            num_layers=opt.num_layer, # opt.num_layer
+                            batch_first=True,
+                            bidirectional=True)
+        self.drop = nn.Dropout(p=opt.dropout) # opt.dropout
+
+        self.fc = nn.Linear(2*opt.hidden_dim, 5)
+
+    def forward(self, input_ids):
+        """
+        Feed input to embedding layer and the classifier to compute logits
+        
+        :param input_ids: an input tensor with shape (batch_size, max_length) [torch.Tensor]
+        
+        :return logits: an output tensor with shape (batch_size, num_class) [torch.Tensor]
+        """
+        # Feed input to embedding layer
+        sent_embeddings_tsr = self.embedding(input_ids) # [batch_size, max_len, embedding_dim] ? 
+#         print(f"LstmClassifier - forward - sent_embeddings_tsr.shape: {sent_embeddings_tsr.shape}")
+        sent_embeddings_tsr = self.drop(sent_embeddings_tsr)
+
+        # Feed embedding to Lstm
+        output, (hidden_states_tsr, cell_states_tsr) = self.lstm(sent_embeddings_tsr) # hidden_states_tsr = [2, batch_size, hidden_size]
+#         print(f"LstmClassifier - forward - hidden_states_tsr.shape: {hidden_states_tsr.shape}")
+
+        # Feed input to classifier to compute logits
+        right_hidden_states_tsr = hidden_states_tsr[-1] # [batch_size, hidden_size]
+#         print(f"LstmClassifier - forward - right_hidden_states_tsr.shape: {right_hidden_states_tsr.shape}")
+        left_hidden_states_tsr = hidden_states_tsr[-2] # [batch_size, hidden_size]
+#         print(f"LstmClassifier - forward - left_hidden_states_tsr.shape: {left_hidden_states_tsr.shape}")
+        final_hidden_states_tsr = torch.cat((right_hidden_states_tsr, left_hidden_states_tsr), dim=1)  # [batch_size, 2*hidden_size]
+#         print(f"LstmClassifier - forward - final_hidden_states_tsr.shape: {final_hidden_states_tsr.shape}")
+
+        logits = self.fc(final_hidden_states_tsr) # [batch_size, 5]
+#         print(f"LstmClassifier - forward - logits.shape: {logits.shape}")
+
+        return logits
