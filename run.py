e3c5228c5fef59c62f85df0bac2f136a76e7c55f
Jae Hyun Lee <jminy8@gmail.com>
2021-05-31 17:46:49 +0900

    Add files via upload

--- run.py
+++ run.py
@@ -34,6 +34,7 @@ from Utils.utils import *
 from Models.BertClf import *
 from Models.LstmClf import *
 from Models.ElectraClf import *
+from Models.ConvClf import *
 
 
 #################################################################################################################
@@ -93,7 +94,7 @@ def train_fn(model,
             model.zero_grad()
 
             # Perform a forward pass. This will return logits.
-            if opt.model == "BILSTM":
+            if opt.model in ["BILSTM", "CNN"]:
                 logits = model(b_ids_tsr)
             else:
                 logits = model(b_ids_tsr, b_masks_tsr)
@@ -202,7 +203,7 @@ def evaluate_fn(model, loss_fn, valid_dataloader):
 
         # Compute logits
         with torch.no_grad():
-            if opt.model == "BILSTM":
+            if opt.model in ["BILSTM", "CNN"]:
                 logits = model(b_ids_tsr)
             else:
                 logits = model(b_ids_tsr, b_masks_tsr)
@@ -318,16 +319,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # model
-    parser.add_argument('--model', type=str, default='ELECTRA', help='BERT, BILSTM, ELECTRA')
+    parser.add_argument('--model', type=str, default='CNN', help='BERT, BILSTM, ELECTRA, CNN')
     parser.add_argument('--sent_embedding', type=int, default=0, help='0: CLS, 1: 4-layer concat')
     parser.add_argument('--hidden_dim', type=int, default=768, help='BERT or ELECTRA: hidden dimension of classifier, BILSTM: hidden dimension of lstm')
     parser.add_argument('--num_layer', type=int, default=2, help='BILSTM: number of layers of lstm')
+    parser.add_argument("--embedding_dim", type=int, default=256, help='embedding dimension of CNN')
+    parser.add_argument("--kernel_sizes", nargs='+', default=[3, 4, 5], type=int, help='kernel sizes of CNN')
+    parser.add_argument("--kernel_depth", default=500, type=int, help='kernel depth of CNN')
     parser.add_argument('--dropout', type=float, default=0.5, help='dropout ratio')
 
     # training
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--gpu', type=int, default=0, help='0,1,2,3')
-    parser.add_argument('--max_epoch', type=int, default=1)
+    parser.add_argument('--max_epoch', type=int, default=50)
     parser.add_argument('--save', type=int, default=1, help='0: false, 1:true')
     parser.add_argument('--lr_pretrained', type=float, default=1e-05, help='learning rate, 5e-5, 3e-5 or 2e-5')
     parser.add_argument('--lr_clf', type=float, default=0.0001, help='learning rate, 5e-5, 3e-5 or 2e-5')
@@ -341,12 +345,12 @@ if __name__ == "__main__":
     parser.add_argument('--save_submission_path', type=str, default='./Submissions')
     parser.add_argument('--max_len', type=int, default=50, help='max length of the sentence')
     parser.add_argument('--aug', type=int, default=0, help='0: false, 1: true(ru)')
-    parser.add_argument('--split_ratio', type=int, default=3, help='k/10, k in [1,2,3]')
+    parser.add_argument('--split_ratio', type=int, default=1, help='k/10, k in [1,2,3]')
     parser.add_argument('--author', type=str, default='jh')
 
 
-    #     opt = parser.parse_args() # in .py env
-    opt, _ = parser.parse_known_args() # in .ipynb env
+    opt = parser.parse_args() # in .py env
+#     opt, _ = parser.parse_known_args() # in .ipynb env
 
     #################################################################################################################
     # Training Device
