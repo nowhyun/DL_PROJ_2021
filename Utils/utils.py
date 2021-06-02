b6662a68c68e45e037134503b4fa05c57a0696be
Jae Hyun Lee <jminy8@gmail.com>
2021-06-02 22:36:44 +0900

    Add files via upload

--- Utils/utils.py
+++ Utils/utils.py
@@ -57,11 +57,12 @@ def initialize_model(opt, len_train_dataloader, device):
     if opt.model == "BERT":
         model = BertClassifier(opt)
         model.to(device)
-
+        
         optimizer = AdamW([
             {"params": model.bert.parameters(), "lr": opt.lr_pretrained, "eps": opt.eps},
             {"params": model.classifier.parameters(), "lr": opt.lr_clf, "eps": opt.eps}
                            ])
+        
         # Total number of training steps
         total_steps = len_train_dataloader * opt.max_epoch
 
@@ -73,11 +74,12 @@ def initialize_model(opt, len_train_dataloader, device):
     elif opt.model == "ELECTRA":
         model = ElectraClassifier(opt)
         model.to(device)
-
+        
         optimizer = AdamW([
             {"params": model.electra.parameters(), "lr": opt.lr_pretrained, "eps": opt.eps},
             {"params": model.classifier.parameters(), "lr": opt.lr_clf, "eps": opt.eps}
                            ])
+        
         # Total number of training steps
         total_steps = len_train_dataloader * opt.max_epoch
 
@@ -103,6 +105,63 @@ def initialize_model(opt, len_train_dataloader, device):
     return model, optimizer, scheduler
 
 
+class SAM(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, rho=0.05, **kwargs):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+
+        defaults = dict(rho=rho, **kwargs)
+        super(SAM, self).__init__(params, defaults)
+
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+
+    @torch.no_grad()
+    def first_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"] / (grad_norm + 1e-12)
+
+            for p in group["params"]:
+                if p.grad is None: continue
+                e_w = p.grad * scale.to(p)
+                p.add_(e_w)  # climb to the local maximum "w + e(w)"
+                self.state[p]["e_w"] = e_w
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None: continue
+                p.sub_(self.state[p]["e_w"])  # get back to "w" from "w + e(w)"
+
+        self.base_optimizer.step()  # do the actual "sharpness-aware" update
+
+        if zero_grad: self.zero_grad()
+
+    @torch.no_grad()
+    def step(self, closure=None):
+        assert closure is not None, "Sharpness Aware Minimization requires closure, but it was not provided"
+        closure = torch.enable_grad()(closure)  # the closure should do a full forward-backward pass
+
+        self.first_step(zero_grad=True)
+        closure()
+        self.second_step()
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  # put everything on the same device, in case of model parallelism
+        norm = torch.norm(
+                    torch.stack([
+                        p.grad.norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                    p=2
+               )
+        return norm
+    
+    
 ################################################################################################################
 # Make Submission
 ################################################################################################################
@@ -191,13 +250,20 @@ def get_sent_embedding_and_label(dataloader, model, batch_size, device):
     """
     sent_embeddings_arr = []
     labels_arr = []
+    n = 0
     for i, batch in enumerate(dataloader):
         b_ids_tsr, b_masks_tsr, b_labels_tsr = tuple(tsrs.to(device) for tsrs in batch)
-        embeddings_tsr = model.extract_sent_embd(b_ids_tsr, b_masks_tsr)
-        sent_embeddings_tsr = embeddings_tsr[:,0,:]
+        if str(model)[:4] == 'Lstm':
+            embeddings_tsr = model.extract_sent_embd(b_ids_tsr)
+            sent_embeddings_tsr = embeddings_tsr
+            n = 0
+        else:
+            embeddings_tsr = model.extract_sent_embd(b_ids_tsr, b_masks_tsr)
+            sent_embeddings_tsr = embeddings_tsr[:,0,:]
+            n = 7
         sent_embeddings_arr.append(sent_embeddings_tsr)
         labels_arr.append(b_labels_tsr)
-        if i == 7:
+        if i == n:
             print(f"the number of sample: {batch_size * (i+1)}")
             break
     sent_embeddings_arr = torch.cat(sent_embeddings_arr, dim=0).detach().cpu().numpy()
